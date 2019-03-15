01ebbc000aa8d3615537318db1410b382bb72758
Karthik-B-06 <karthik06051996@gmail.com>
2019-03-15 18:20:24 +0530

    added dist folder

--- server.js
+++ server.js
@@ -3,5 +3,7 @@ const serveStatic = require("serve-static")
 const path = require('path');
 app = express();
 app.use(serveStatic(path.join(__dirname, 'dist')));
-const port = process.env.PORT || 80;
-app.listen(port);
+const port = process.env.PORT || 8080;
+app.listen(port, () => {
+    console.log("Listening in port", port)
+});
